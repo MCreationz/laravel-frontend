121a5378742fa5cfd04083f9bde691288a13b8e0
Vinay Kaushik <vinay.swapit@gmail.com>
2026-03-13 15:30:42 +0530

    layout js

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -171,43 +171,7 @@
             }
         }
     </script>
-    <script>
-        document.querySelectorAll(".select-wrapper").forEach(function(wrapper) {
 
-            const selectBox = wrapper.querySelector(".custom-select");
-            const optionsList = wrapper.querySelector(".select-list");
-            const hiddenInput = wrapper.querySelector(".hidden-select");
-
-            selectBox.addEventListener("click", function(e) {
-                e.stopPropagation();
-
-                document.querySelectorAll(".select-list").forEach(list => {
-                    if (list !== optionsList) list.style.display = "none";
-                });
-
-                optionsList.style.display =
-                    optionsList.style.display === "block" ? "none" : "block";
-            });
-
-            optionsList.querySelectorAll("li").forEach(function(option) {
-
-                option.addEventListener("click", function() {
-
-                    selectBox.textContent = this.textContent;
-                    hiddenInput.value = this.getAttribute("data-value");
-
-                    optionsList.style.display = "none";
-                });
-
-            });
-
-        });
-
-        document.addEventListener("click", function() {
-            document.querySelectorAll(".select-list").forEach(list => {
-                list.style.display = "none";
-            });
-        });
-    </script>
+   
 
 @endsection
